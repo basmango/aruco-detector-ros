7344bed1028323eb403500f5404be4eaa5a31099
basmango <bassampervez@gmail.com>
2023-02-22 15:06:34 +0530

    added desired drawing

--- aruco.py
+++ aruco.py
@@ -13,7 +13,8 @@ class ArucoDetector:
         self.image_sub = rospy.Subscriber('/video_frames', Image, self.image_callback)
         self.image_pub = rospy.Publisher('/detected_aruco_frames', Image, queue_size=10)
         self.coord_pub = rospy.Publisher('/aruco_coordinates', Int32MultiArray, queue_size=10)
-
+        self.self.pt_star = np.array([566.00, 283.00, 719.00, 326.00,675.00, 480.00,520.0,435.0]) #Desired pixel points location
+    
     def image_callback(self, msg):
         cv_image = self.bridge.imgmsg_to_cv2(msg, desired_encoding='bgr8')
         aruco_dict = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_4X4_250)
@@ -30,10 +31,10 @@ class ArucoDetector:
                 ptC = (int(ptC[0]), int(ptC[1]))
                 ptD = (int(ptD[0]), int(ptD[1]))
                 ptA = (int(ptA[0]), int(ptA[1]))
-                #cv2.line(cv_image, ptA, (pt_star[0],pt_star[1]), (0, 255, 0), 2)
-                #cv2.line(cv_image, ptB, (pt_star[2],pt_star[3]), (0, 255, 0), 2)
-                #cv2.line(cv_image, ptC, (pt_star[4],pt_star[5]), (0, 255, 0), 2)
-                #cv2.line(cv_image, ptD, (pt_star[6],pt_star[7]), (0, 255, 0), 2)
+                cv2.line(cv_image, ptA, (self.pt_star[0],self.pt_star[1]), (0, 255, 0), 2)
+                cv2.line(cv_image, ptB, (self.pt_star[2],self.pt_star[3]), (0, 255, 0), 2)
+                cv2.line(cv_image, ptC, (self.pt_star[4],self.pt_star[5]), (0, 255, 0), 2)
+                cv2.line(cv_image, ptD, (self.pt_star[6],self.pt_star[7]), (0, 255, 0), 2)
                 # draw the center (x, y)-coordinates of the AprilTag
                 (cX, cY) = (int((ptA[0]+ptC[0])/2), int((ptA[1]+ptC[1])/2))
                 cv2.circle(cv_image, (cX, cY), 10, (0, 0, 255), -1)
@@ -42,7 +43,7 @@ class ArucoDetector:
                 cv2.circle(cv_image, ptC, 5, (255, 0, 0), -1)
                 cv2.circle(cv_image, ptD, 5, (255, 0, 0), -1)
 
-
+            
             aruco_coords = [ptA[0], ptA[1], ptB[0], ptB[1], ptC[0], ptC[1], ptD[0], ptD[1], cX, cY]
             # convert the corner coordinates to text
             #corners_text = f"({ptA[0]:.2f}, {ptA[1]:.2f}), ({ptB[0]:.2f}, {ptB[1]:.2f}), ({ptC[0]:.2f}, {ptC[1]:.2f}), ({ptD[0]:.2f}, {ptD[1]:.2f})"
@@ -57,11 +58,11 @@ class ArucoDetector:
 
 
 
-            #cv2.circle(cv_image, ((pt_star[0]+pt_star[2]+pt_star[4]+pt_star[6])/4,(pt_star[1]+pt_star[3]+pt_star[5]+pt_star[7])/4), 5, (0, 255, 0), -1)
-            #cv2.circle(cv_image, (pt_star[0],pt_star[1]), 3, (0, 255, 0), -1)
-            #cv2.circle(cv_image, (pt_star[2],pt_star[3]), 3, (0, 255, 0), -1)
-            #cv2.circle(cv_image, (pt_star[4],pt_star[5]), 3, (0, 255, 0), -1)
-            #cv2.circle(cv_image, (pt_star[6],pt_star[7]), 3, (0, 255, 0), -1)
+            cv2.circle(cv_image, ((self.self.pt_star[0]+self.pt_star[2]+self.pt_star[4]+self.pt_star[6])/4,(self.pt_star[1]+self.pt_star[3]+self.pt_star[5]+self.pt_star[7])/4), 5, (0, 255, 0), -1)
+            cv2.circle(cv_image, (self.pt_star[0],self.pt_star[1]), 3, (0, 255, 0), -1)
+            cv2.circle(cv_image, (self.pt_star[2],self.pt_star[3]), 3, (0, 255, 0), -1)
+            cv2.circle(cv_image, (self.pt_star[4],self.pt_star[5]), 3, (0, 255, 0), -1)
+            cv2.circle(cv_image, (self.pt_star[6],self.pt_star[7]), 3, (0, 255, 0), -1)
             #cv_image = aruco.drawDetectedMarkers(cv_image, corners, ids)
         
         self.image_pub.publish(self.bridge.cv2_to_imgmsg(cv_image, encoding='bgr8'))
